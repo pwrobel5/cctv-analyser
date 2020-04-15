f58830f74f1f52bbf337413139aefbc0e559d671
Piotr Wróbel <pwrobel5@gmail.com>
2020-04-16 01:10:09 +0200

    play/stop video, implemented video capture

--- gui/app.py
+++ gui/app.py
@@ -1,12 +1,20 @@
 import tkinter
 import tkinter.filedialog as filedialog
 
+import PIL.Image
+import PIL.ImageTk
+
+from .video_capture import VideoCapture
+
 
 class App:
     def __init__(self, window, window_title):
         self.window = window
         self.window.title(window_title)
+
         self.video_source = None
+        self.photo = None
+        self.play_video = False
 
         # Canvas for video display
         self.canvas = tkinter.Canvas(window, width=800, height=680)
@@ -24,18 +32,45 @@ class App:
         self.analyse_button = tkinter.Button(window, text="Analyse video", command=self.analyse)
         self.analyse_button.pack(side=tkinter.RIGHT)
 
+        self.delay = 15
+        self.update()
+
         self.window.mainloop()
 
     def browse(self):
         path = filedialog.askopenfilename()
         if path:
-            self.video_source = path
+            try:
+                self.video_source = VideoCapture(path)
+            except ValueError:
+                return
+
+            width = self.video_source.width
+            height = self.video_source.height
+            self.canvas.config(width=width, height=height)
+
+            ret, frame = self.video_source.get_frame()
+            if ret:
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
 
     def play(self):
-        pass
+        self.play_video = True
+        self.update()
 
     def stop(self):
-        pass
+        self.play_video = False
 
     def analyse(self):
         pass
+
+    def update(self):
+        # Get a frame from the video source
+        if self.play_video:
+            ret, frame = self.video_source.get_frame()
+
+            if ret:
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+
+            self.window.after(self.delay, self.update)
