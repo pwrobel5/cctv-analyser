ca5c0ce908d9a0b55825d109a0f665230a3878f2
Piotr Wróbel <pwrobel5@gmail.com>
2020-06-07 20:32:17 +0200

    refresh reference frame

--- tools/analyse.py
+++ tools/analyse.py
@@ -11,11 +11,13 @@ import numpy as np
           minimum area to detect motion
           maybe it's better to use running average rather than reference frame
           alpha for running average
+          refresh reference frequency
 """
 
 
 class Analyser:
-    def __init__(self, reference_frame, blur_size=21, move_threshold=25, dilation_iterations=2, use_running_average=True, alpha=0.020):
+    def __init__(self, reference_frame, blur_size=21, move_threshold=25, dilation_iterations=2,
+                 use_running_average=True, alpha=0.020):
         # TODO - what does this 0 mean?
         self._reference_frame = cv2.GaussianBlur(reference_frame, (blur_size, blur_size), 0)
         self._minimal_area = 400
@@ -25,10 +27,13 @@ class Analyser:
         self._use_running_average = use_running_average
         self._running_average = np.float32(reference_frame)
         self._average_alpha = alpha
+        self._refresh_reference_frequency = 100
+        self._counter = 0
 
     def analyse_frame(self, frame):
         text = "No motion"
         analysed_frame = cv2.GaussianBlur(frame, (self._blur_size, self._blur_size), 0)
+        self._counter += 1
 
         if self._use_running_average:
             cv2.accumulateWeighted(analysed_frame, self._running_average, 0.020, None)  # alpha could be parametrised
@@ -64,6 +69,12 @@ class Analyser:
 
         cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 0, 255), 2)
+
+        if not motion_detected and self._counter >= self._refresh_reference_frequency:
+            self._reference_frame = analysed_frame
+            self._counter = 0
+            print("Changed reference frame")
+
         return frame, motion_detected
 
     @property
@@ -72,4 +83,4 @@ class Analyser:
 
     @reference_frame.setter
     def reference_frame(self, reference_frame):
-        self._reference_frame = reference_frame
+        self._reference_frame = cv2.GaussianBlur(reference_frame, (self._blur_size, self._blur_size), 0)
