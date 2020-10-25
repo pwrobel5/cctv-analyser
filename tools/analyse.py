4657ba6a569eb19b830fa6c61264ef22ebe1e9bf
Piotr Wróbel <pwrobel5@gmail.com>
2020-10-25 20:36:20 +0100

    using background subtraction from openCV library

--- tools/analyse.py
+++ tools/analyse.py
@@ -1,15 +1,17 @@
 import cv2
 import numpy as np
-import imutils
 
 
 class Analyser:
-    def __init__(self, reference_frame, parameters, frames_to_average=None):
+    def __init__(self, reference_frame, parameters, frames_to_average=None, bg_subtractor="KNN"):
         self._parameters = parameters
         self._reference_frame = self.__preprocess_frame(reference_frame)
         self._running_average = self.__calculate_average_frame(frames_to_average)
         self._frame_counter = 0
-        self._background_subtractor = cv2.createBackgroundSubtractorMOG2()
+        if bg_subtractor == "KNN":
+            self._background_subtractor = cv2.createBackgroundSubtractorKNN(detectShadows=False)
+        else:
+            self._background_subtractor = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 
     def __calculate_average_frame(self, frames):
         if not frames:
@@ -25,7 +27,6 @@ class Analyser:
         return result
 
     def __preprocess_frame(self, frame):
-        #frame = imutils.resize(frame, width=200)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # TODO - what does this 0 mean?
         frame = cv2.GaussianBlur(frame, (self._parameters.blur_size, self._parameters.blur_size), 0)
@@ -33,7 +34,10 @@ class Analyser:
 
     def analyse_frame(self, frame):
         text = "No motion"
-        bg_mask = self._background_subtractor.apply(frame)
+        frame_copy = frame.copy()
+        frame_copy = cv2.GaussianBlur(frame_copy, (self._parameters.blur_size, self._parameters.blur_size), 0)
+        bg_mask = self._background_subtractor.apply(frame_copy)
+        # cv2.imshow(frame, "aa")
         analysed_frame = self.__preprocess_frame(frame)
         self._frame_counter += 1
 
@@ -58,12 +62,11 @@ class Analyser:
 
         cv2.imshow("Threshold", threshold)
         cv2.imshow("Frame delta", frame_delta)
-        cv2.imshow("After bg subtraction", bg_mask)
-        # OpenCV needs it to correctly show images for some reason
-        # (https://stackoverflow.com/questions/21810452/cv2-imshow-command-doesnt-work-properly-in-opencv-python/50947231)
-        cv2.waitKey(1)
+
+        bg_threshold = cv2.threshold(bg_mask, self._parameters.delta_threshold, 255, cv2.THRESH_BINARY)[1]
 
         contours = cv2.findContours(threshold.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        contours_bg = cv2.findContours(bg_threshold.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         # length of contours and position of needed element to read is dependent on OpenCV version
         if len(contours) == 2:
             contours = contours[0]
@@ -73,24 +76,57 @@ class Analyser:
             print("Error")
             return None
 
-        motion_detected = False
+        if len(contours_bg) == 2:
+            contours_bg = contours_bg[0]
+        elif len(contours_bg) == 3:
+            contours_bg = contours_bg[1]
+        else:
+            print("Error")
+            return None
 
-        for contour in contours:
-            if cv2.contourArea(contour) < self._parameters.minimal_move_area:
+        """
+        bg_text = "No motion"
+
+       for contour_bg in contours_bg:
+            if cv2.contourArea(contour_bg) < self._parameters.minimal_move_area:
                 continue
 
-            (x, y, w, h) = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Motion detected"
-            motion_detected = True
+            (x, y, w, h) = cv2.boundingRect(contour_bg)
+            cv2.rectangle(bg_mask, (x,y), (x + w, y + h), (0, 255, 0), 2)
+            bg_text = "Motion detected"
+
+        cv2.putText(bg_mask, "Status: {}".format(bg_text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 0, 255), 2)"
+        """
+
+        cv2.imshow("After bg subtraction", bg_threshold)
+        # OpenCV needs it to correctly show images for some reason
+        # (https://stackoverflow.com/questions/21810452/cv2-imshow-command-doesnt-work-properly-in-opencv-python/50947231)
+        #cv2.waitKey(1)
+
+        motion_detected = False
+        found_contours = len(contours_bg)
+        contours_bg = list(filter(lambda cnt: cv2.contourArea(cnt) >= self._parameters.minimal_move_area, contours_bg))
+
+        if found_contours < 500:
+            for contour in contours_bg:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                text = "Motion detected"
+                motion_detected = True
 
         cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 0, 255), 2)
+        if motion_detected:
+            print("Contours: ", found_contours)
 
         if not motion_detected and self._frame_counter >= self._parameters.reference_frame_refresh_frequency:
             self._reference_frame = analysed_frame
             self._frame_counter = 0
             print("Changed reference frame")
+
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
 
         return frame, motion_detected
 
